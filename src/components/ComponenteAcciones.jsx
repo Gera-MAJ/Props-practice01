48ece381427dfa6d6e074599ee8f0b81b1831e58
Gerardo <jatipg@gmail.com>
2024-01-10 02:10:49 -0300

    todos los ejemplos terminados

--- src/components/ComponenteAcciones.jsx
+++ src/components/ComponenteAcciones.jsx
@@ -6,11 +6,31 @@ export const ComponenteAcciones = () => {
     console.log(`${tipo} en la caja`)
   }
 
+  const estasDentro = e => {
+    console.log("Estas dentro del input")
+  }
+
+  const estasFuera = e => {
+    console.log("Estas fuera del input")
+  }
+
+  const alertarAlgo = () => {
+    alert("Estas presionando el botón") 
+  }
 
   return (
+    // usamos las acciones de mouseenter y mouse leave
     <div id='caja' onMouseEnter={ e => Alertar(e, "Entraste") } onMouseLeave={ e => Alertar(e, "Saliste") }>
+        {/* Aplicamos las acciones de Focus y Blur*/}
+        <input type="text" 
+          onFocus={ estasDentro }
+          onBlur={ estasFuera }
+        />
 
+        <button onClick={() => {alertarAlgo()}}>Presiona Aquí</button>
     </div>
+
+    //La diferencia que tien cada una de las acciones, es que unas detectan el objeto donde están y las otras debo hacer click para que sepa que cosa estoy ejecutando
   )
 }
 
